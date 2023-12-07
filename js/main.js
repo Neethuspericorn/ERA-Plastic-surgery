ca51d5efecd2ffa1e5822fa960509035551885e8
Niyas MN <niyas.mn@spericorn.com>
2023-12-07 11:17:16 +0530

    slider fade updates

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 $(document).ready(function(){
     $('.banner-slider').slick({
         arrows: false,
+        fade: true
     });
     $('#home-tt-slider').slick({
         dots: true,
         prevArrow: "#prev",
-        nextArrow: "#next"
+        nextArrow: "#next",
+        fade: true
     });
     $('.testimonial-slider').slick({
         dots: true,
         prevArrow: "#prevArrow",
-        nextArrow: "#nextArrow"
+        nextArrow: "#nextArrow",
+        fade: true
     });
     $('.tranform-slide').slick({
-        arrows: true
+        arrows: true,
+        fade: true
     })
 });
 
